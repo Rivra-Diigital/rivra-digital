8d8086de44e38037ec23f94cd1efcc4cb9fa9d3b
Pedro Ecsedei <pedro.martins55757@gmail.com>
2025-09-21 18:08:07 -0300

    chore: content adjustment

--- src/view/Home/Hero/index.tsx
+++ src/view/Home/Hero/index.tsx
@@ -7,12 +7,13 @@ export const HomeHero = () => {
       <div className="container h-screen relative z-20 flex flex-col justify-center">
         <div className="flex flex-col w-full md:max-w-[611px]">
           <h1 className="text-neutral-50 font-bold text-2xl sm:text-[42px] w-full text-center md:text-left">
-            Desenvolvemos a sua necessidade.
+            Entregamos soluções e identidade visual para o seu negócio.
           </h1>
 
           <Button
+            url="#quem-somos"
             className="mx-auto md:mx-0 mt-4"
-            content="Conheça nosso trabalho"
+            content="Conheça a Rivra"
             color="green"
             icon={
               <svg
